8cfafacddbd6e4f5c2ce7b5905bfdd36fcaf44d4
Danial Baharvand <dbdpcg@gmail.com>
2022-04-25 03:35:59 +1000

    Added email functionality, cleaned up the code

--- main.py
+++ main.py
@@ -3,43 +3,62 @@
 # Below are various import statements that are used in the script
 #
 # Import the function for opening online documents
-
 from urllib.request import urlopen
-# import library for reading tables from PDF files
-
-from tabula import read_pdf, convert_into
-from tabulate import tabulate
-
-
-#import camelot
-
-from tabulate import tabulate
+#
+# Import library for reading tables from PDF files
+from tabula import read_pdf
+#
+# Import library for sending emails
+import smtplib, ssl
+#
+# -----Options--------------------------------------------------------#
+#
+# Name to be searched on Brisbane court notice
+searchWord = "Search Name"
+#
+# The address of the document to be searched
+pdf_path = "https://www.courts.qld.gov.au/__external/CourtsLawList/BrisbaneMagCourt.pdf"
+#
+# Name of the saved file
+fileName = "names"
 # --------------------------------------------------------------------#
 
+def send_mail():
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "senderMail@gmail.com"  # Where the email is going to be sent from
+    receiver_email = "receiverMail@gmail.com"  # Where the email is going to be received
+    password = "yourPassword"
+    message = """\
+    Subject: Brisbane court alert
+
+    A new match has been found."""
+    # Create SSL connection
+    context = ssl.create_default_context()
+    # Initiate the SMTP server
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        # send the e-mail
+        server.sendmail(sender_email, receiver_email, message)
 
 
 def download_file(download_url, filename):
+    # Download a PDF file from a URL and store it on storage
     response = urlopen(download_url)
     file = open(filename + ".pdf", 'wb')
     file.write(response.read())
     file.close()
 
 
-def extractInfo (srcPath, destPath): # Extracts info from a file at the given file path
-    tables = read_pdf(srcPath, pages="all")
-    # convert_into(srcPath, destPath)
-    return tables
 
 
-searchWord = "Abdi"
-pdf_path = "https://www.courts.qld.gov.au/__external/CourtsLawList/BrisbaneMagCourt.pdf"
-download_file(pdf_path, "names")
-tables = read_pdf('namesLocal.pdf', pages="all")
+download_file(pdf_path, fileName)
+tables = read_pdf(fileName, pages="all")
 for table in tables:
     if 'Matter' in table:
         for name in table['Matter']:
-            print(name)
             if isinstance(name, str):
                 if searchWord in name:
-                    print("yes")
+                    print("Found a match!")
+                    send_mail()
 
